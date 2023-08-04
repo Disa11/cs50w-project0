3a48a1f5377858953878fd3d57cf8c43b117e96b
Disa11 <stevenknauth3@gmail.com>
2023-08-04 17:37:54 -0600

    a

--- static/script.js
+++ static/script.js
@@ -1,11 +1,14 @@
-var preloader = document.getElementById("preloader");
-const element = document.getElementsByClassName("tagvisibility");
 
-window.addEventListener("load", () => {
+function loader() {
   preloader.style.display = "none";
   document.getElementById('body_id').classList.remove('hidden');
 
   for (let i = 0; i < element.length; i++) {
     element[i].style.visibility = "visible"
   }
-})
+}
+
+var preloader = document.getElementById("preloader");
+const element = document.getElementsByClassName("tagvisibility");
+
+window.addEventListener("load", setTimeout(loader, 1000)); //Funcion para mostrar aunque sea un segundo la animacion xd ya que la pagina aun no e smuy pesada
